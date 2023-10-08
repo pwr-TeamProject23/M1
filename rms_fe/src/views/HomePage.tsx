7058a0bd1d88232b3cbf99a1f9eff2870b43e70b
Philip Policki <philip.policki@gmail.com>
2023-10-08 17:50:31 +0200

    feat(frontend): initial frontend app

---
+++ rms_fe/src/views/HomePage.tsx
@@ -0,0 +1,68 @@
+import { Card, Space } from "antd"
+import * as React from "react"
+
+export const HomePage = () => {
+    return (
+        <Space>
+            <Card title="Reviewers Matching System">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vehicula rutrum vulputate. Aliquam non
+                mi sollicitudin, gravida massa faucibus, blandit urna. Aliquam erat volutpat. Lorem ipsum dolor sit
+                amet, consectetur adipiscing elit. Nunc vitae lorem sed ex rhoncus sollicitudin in nec augue. Donec
+                efficitur, justo ut aliquet egestas, purus ex vestibulum urna, et posuere felis ante ultricies risus.
+                Praesent egestas vulputate venenatis. Proin eget sagittis justo. Nulla eget bibendum turpis, ut vehicula
+                enim. Etiam lobortis dapibus erat, in mollis nulla consectetur ac. Donec faucibus enim quis laoreet
+                tincidunt. Donec mollis arcu ex, non tempus turpis pharetra a. Cras ac lectus eget nulla euismod
+                aliquet. Sed scelerisque faucibus erat eget suscipit. Aenean semper, enim at luctus finibus, est risus
+                ullamcorper lorem, et accumsan tortor orci vel purus. Lorem ipsum dolor sit amet, consectetur adipiscing
+                elit. Nulla facilisi. Phasellus congue elementum eros. Curabitur in tincidunt risus, sed blandit neque.
+                Nulla placerat tincidunt purus, vitae consectetur quam pellentesque venenatis. Nam at nisi a turpis
+                pellentesque suscipit. Nullam accumsan turpis ut diam tempor, eu ultrices mi mollis. Pellentesque
+                facilisis lectus non ex lacinia, sed tempus ex imperdiet. Aenean imperdiet est eget vestibulum maximus.
+                Integer porta varius est, et lacinia eros vehicula eget. Curabitur vitae turpis maximus, suscipit urna
+                in, luctus velit. Nulla congue libero feugiat nisl placerat ullamcorper. Sed tempor suscipit rhoncus.
+                Aliquam cursus lectus in justo molestie pretium. Duis rhoncus nisl eu ex scelerisque fringilla. Quisque
+                dignissim facilisis finibus. Aliquam pretium laoreet turpis id rutrum. Nullam vestibulum vestibulum
+                mollis. Aliquam eu urna ut justo tempor dapibus. Sed quis dictum sem. Praesent pretium nisi a eleifend
+                facilisis. Pellentesque sed neque in augue vulputate dictum. Nunc varius vestibulum rutrum. In malesuada
+                pretium sollicitudin. Quisque eleifend ut erat at aliquet. Vivamus mauris turpis, pellentesque at
+                tristique vel, ullamcorper eu elit. Fusce vitae est non dui feugiat ornare nec sit amet lorem. Maecenas
+                ut blandit urna. Curabitur faucibus leo leo, ac luctus velit finibus sed. In pellentesque sem nec
+                molestie cursus. Maecenas at nisi in nibh mollis lacinia sed ac mi. Donec nec ipsum aliquet, porta augue
+                eu, dignissim nunc. Sed in diam convallis, pellentesque odio non, rutrum risus. Proin justo nulla,
+                mattis ac ultricies non, aliquam ut ante. Morbi vel augue lobortis, interdum velit ac, auctor elit.
+                Curabitur vitae auctor massa, sit amet accumsan justo. Vivamus volutpat mollis mi, ut finibus tellus
+                pulvinar in. Duis lobortis vel nulla et lobortis. Maecenas ultricies, ante vel sollicitudin dignissim,
+                ex neque sagittis urna, tempor elementum dolor eros quis ex. Integer eget tincidunt massa. Vestibulum
+                ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Fusce consectetur massa sed
+                sem lacinia fermentum. Quisque mauris odio, aliquam lacinia aliquet suscipit, pretium eu justo. Nunc
+                tempus dui nec ornare efficitur. Donec vehicula arcu sit amet tortor volutpat, sit amet auctor quam
+                dictum. Aliquam tempor vel ex ac iaculis. Sed cursus eros a nulla accumsan congue. Duis malesuada
+                egestas commodo. Sed mollis turpis et viverra venenatis. Quisque in finibus orci. Orci varius natoque
+                penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed vehicula, arcu non venenatis
+                gravida, purus lorem mattis metus, a molestie urna nunc a augue. Sed fringilla lorem sit amet
+                scelerisque commodo. Nulla nunc quam, laoreet vel purus sed, congue pulvinar dolor. Aenean lacinia
+                bibendum dolor vitae consectetur. Donec lacinia porttitor consectetur. Fusce facilisis tortor at quam
+                interdum, vehicula egestas eros aliquet. Duis ut tempus mi, sed placerat nulla. Proin posuere eros sit
+                amet hendrerit ullamcorper. Morbi ac urna in sapien hendrerit mollis. Sed et massa euismod, viverra
+                velit eget, iaculis eros. Nunc scelerisque tortor eu sagittis tincidunt. Proin sodales ligula id risus
+                auctor, a molestie tellus commodo. Nullam maximus elit nunc. Integer quis neque convallis, condimentum
+                sapien sit amet, lacinia elit. Vivamus gravida molestie nisi, vitae pretium arcu suscipit at. Donec in
+                nisl in lorem facilisis sagittis ac sit amet magna. Ut vel porttitor ipsum, eu suscipit quam. Morbi
+                rutrum, urna et fermentum tincidunt, urna nibh malesuada lacus, id mattis velit ante eget metus.
+                Maecenas ullamcorper non nulla a finibus. Aliquam viverra, metus at lacinia fringilla, libero nunc
+                tristique metus, lacinia rhoncus mi velit at arcu. Praesent vehicula ipsum vitae libero scelerisque
+                fringilla. Fusce dictum egestas ipsum aliquet pharetra. Donec in mi justo. Vestibulum placerat mollis ex
+                a convallis. Vivamus elementum elit in tortor iaculis vulputate. Suspendisse potenti. Fusce convallis
+                erat arcu, molestie dignissim nulla euismod tincidunt. Vestibulum ac porttitor ligula, eget vestibulum
+                quam. Nam nec nisi at risus faucibus porttitor condimentum sed enim. Orci varius natoque penatibus et
+                magnis dis parturient montes, nascetur ridiculus mus. Pellentesque augue leo, ultrices nec est eget,
+                tincidunt porttitor augue. Mauris id eleifend augue. Nulla tincidunt, diam et pellentesque condimentum,
+                ligula sapien dapibus est, vel sollicitudin diam purus nec orci. Cras vel placerat erat, congue auctor
+                nibh. Quisque lobortis dapibus consequat. Etiam odio neque, blandit in tellus ut, tincidunt pulvinar
+                felis. Nunc vel consequat massa, ut congue lectus. Fusce volutpat ultricies ante eget condimentum. Sed
+                ornare maximus lacus in aliquet. Mauris at iaculis sapien, eu facilisis ligula. Etiam semper convallis
+                magna non sodales. Donec at enim quis elit tincidunt faucibus ut id sem. Aliquam a enim ligula.
+            </Card>
+        </Space>
+    )
+}
