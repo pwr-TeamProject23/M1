81650d089e58aae394c2e109081e333213eef899
jankrokos <jankrokos@gmail.com>
2023-10-09 12:18:41 +0200

    feat(frontend): create login page

--- rms_fe/src/router.tsx
+++ rms_fe/src/router.tsx
@@ -1,10 +1,15 @@
 import * as React from "react"
 import { createBrowserRouter } from "react-router-dom"
 import { HomePage } from "./views/HomePage.tsx"
+import LoginPage from "./views/LoginPage/LoginPage.tsx"
 
 export const router = createBrowserRouter([
     {
         path: "/",
         element: <HomePage />,
+    },
+    {
+        path: "/login",
+        element: <LoginPage />,
     },
 ])
