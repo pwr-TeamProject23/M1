f517deecb53cd1ab02e9823973a44c701c1eec2f
Jakub Syska <jsyska@it-dev.pl>
2023-10-15 16:11:15 +0200

    feat(be/file_processing): implement file related db schema

--- rms/file_processing/models.py
+++ rms/file_processing/models.py
@@ -1,6 +1,21 @@
 from pydantic import BaseModel
 from fastapi import UploadFile
 from abc import ABC, abstractmethod
+from datetime import datetime
+from sqlalchemy.orm import Mapped, mapped_column
+
+from rms.utils.postgres import BaseModel as PostgresBaseModel
+
+class File(PostgresBaseModel):
+    __tablename__ = "file"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str]
+    path: Mapped[str]
+    uploaded_at: Mapped[datetime]
+
+    def __repr__(self) -> str:
+        return f"File({self.name})"
 
 
 class FileUploadResult(BaseModel):
