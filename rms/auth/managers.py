c01c1b06808d8a6617404296ca9caa3e5ebfa719
jankrokos <jankrokos@gmail.com>
2023-10-23 10:24:32 +0200

    feat(auth): implement user creation method

--- rms/auth/managers.py
+++ rms/auth/managers.py
@@ -1,3 +1,5 @@
+import bcrypt
+
 from rms.auth.models import User, UserCookie
 from rms.utils.managers import BaseModelManager
 
@@ -8,6 +10,12 @@ class UserManager(BaseModelManager[User]):
     @classmethod
     def find_by_email(cls, email: str) -> User | None:
         return cls.find_by_attribute("email", email)
+
+    @classmethod
+    def create_user(cls, email: str, password: str, first_name: str, last_name: str) -> User:
+        hashed_password = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt()).decode("utf-8")
+        user = User(email=email, password=hashed_password, first_name=first_name, last_name=last_name)
+        return cls.create(user)
 
 
 class UserCookieManager(BaseModelManager[UserCookie]):
--- rms/auth/views.py
+++ rms/auth/views.py
@@ -1,26 +1,28 @@
 from fastapi import APIRouter, Request, Response, HTTPException, status
 
+import json
+
 from rms.auth.services import LoginRequest, validate_credentials, invalidate_cookie
 
 router = APIRouter()
 
 
 @router.post("/login")
-def login(login_data: LoginRequest):
+def login(login_data: LoginRequest) -> Response:
     result = validate_credentials(login_data.email, login_data.password)
 
     if not result:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid email or password.")
 
     user, auth_cookie = result
-    response = Response(content="Login successful")
+    response = Response(content=json.dumps({"status": "Logged in successfully"}), media_type="application/json")
     response.set_cookie(key="auth_cookie", value=auth_cookie.value, httponly=True, samesite="strict")
 
     return response
 
 
 @router.post("/logout")
-def logout(request: Request):
+def logout(request: Request) -> Response:
     auth_cookie_value = request.cookies.get("auth_cookie")
     if not auth_cookie_value:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not Authorized")
@@ -28,7 +30,7 @@ def logout(request: Request):
     if not invalidate_cookie(auth_cookie_value):
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not Authorized")
 
-    response = Response(content="Logout successful")
+    response = Response(content=json.dumps({"status": "Logged out successfully"}), media_type="application/json")
     response.delete_cookie(key="auth_cookie")
 
     return response
