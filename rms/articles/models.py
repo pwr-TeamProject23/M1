53808b79663c68db3aa1902bca0e5ad9ba2f9a69
jankrokos <jankrokos@gmail.com>
2023-11-22 10:23:22 +0100

    feat(be/articles): add creator data to articles

--- rms/articles/models.py
+++ rms/articles/models.py
@@ -1,8 +1,9 @@
 from datetime import datetime
 
 from sqlalchemy import ForeignKey
-from sqlalchemy.orm import mapped_column, Mapped
+from sqlalchemy.orm import mapped_column, Mapped, relationship
 
+from rms.auth.models import UserOrm
 from rms.utils.postgres import BaseModel
 
 
@@ -15,3 +16,5 @@ class ArticleOrm(BaseModel):
     created_at: Mapped[datetime]
 
     file_id: Mapped[int] = mapped_column(ForeignKey("file.id"))
+    creator_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    creator = relationship(UserOrm, backref="articles")
