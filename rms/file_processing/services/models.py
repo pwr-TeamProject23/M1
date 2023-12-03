5f066db657e5a9e04e83a035219790f38d7eb6e2
Jakub Syska <jsyska@it-dev.pl>
2023-12-03 15:11:14 +0100

    feat(fe): updated article features view to show author's email

--- rms/file_processing/services/models.py
+++ rms/file_processing/services/models.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel
 class Author(BaseModel):
     first_name: str
     last_name: str
-    email: str = None
+    email: str | None = None
 
 
 class PdfArticleData(BaseModel):
