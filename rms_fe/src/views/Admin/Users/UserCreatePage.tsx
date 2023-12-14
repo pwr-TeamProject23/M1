dd0b571d5ca3ab85085e62674d15fe8d40084998
Jakub Syska <jsyska@it-dev.pl>
2023-12-14 18:28:44 +0100

    feat(fe): added back button to pages

--- rms_fe/src/views/Admin/Users/UserCreatePage.tsx
+++ rms_fe/src/views/Admin/Users/UserCreatePage.tsx
@@ -2,6 +2,7 @@ import { Form, Input, Button, message, Flex, Typography } from 'antd';
 import { useNavigate } from 'react-router-dom';
 import { createUser } from '../../../clients/users.ts'; // This should be your API call function
 import { CreateUser } from '../../../types/api/user.ts';
+import { LeftCircleOutlined } from '@ant-design/icons';
 
 export const UserCreatePage = () => {
     const navigate = useNavigate();
@@ -17,29 +18,39 @@ export const UserCreatePage = () => {
     };
 
     return (
-        <Flex style={{ width: "100vw", height: "100vh", justifyContent: "center", alignItems: "center" }}>
-            <Flex>
-                <Flex vertical>
-                    <Typography.Title>Create new user</Typography.Title>
-                    <Form layout="vertical" onFinish={onFinish}>
-                        <Form.Item label="First Name" name="first_name" rules={[{ required: true }]}>
-                            <Input />
-                        </Form.Item>
-                        <Form.Item label="Last Name" name="last_name" rules={[{ required: true }]}>
-                            <Input />
-                        </Form.Item>
-                        <Form.Item label="Email" name="email" rules={[{ required: true, type: 'email' }]}>
-                            <Input />
-                        </Form.Item>
-                        <Form.Item label="Password" name="password" rules={[{ required: true }]}>
-                            <Input.Password />
-                        </Form.Item>
-                        <Form.Item>
-                            <Button block type="primary" htmlType="submit">Create User</Button>
-                        </Form.Item>
-                    </Form>
+        <div>
+            <Button
+                size="large"
+                style={{ margin: "2em 0em 0em 2em", maxWidth: "100px"}}
+                icon={<LeftCircleOutlined />}
+                onClick={() => navigate(-1)}
+            >
+                Back
+            </Button>
+            <Flex style={{ width: "100vw", height: "80vh", justifyContent: "center", alignItems: "center" }}>
+                <Flex>
+                    <Flex vertical>
+                        <Typography.Title>Create new user</Typography.Title>
+                        <Form layout="vertical" onFinish={onFinish}>
+                            <Form.Item label="First Name" name="first_name" rules={[{ required: true }]}>
+                                <Input />
+                            </Form.Item>
+                            <Form.Item label="Last Name" name="last_name" rules={[{ required: true }]}>
+                                <Input />
+                            </Form.Item>
+                            <Form.Item label="Email" name="email" rules={[{ required: true, type: 'email' }]}>
+                                <Input />
+                            </Form.Item>
+                            <Form.Item label="Password" name="password" rules={[{ required: true }]}>
+                                <Input.Password />
+                            </Form.Item>
+                            <Form.Item>
+                                <Button block type="primary" htmlType="submit">Create User</Button>
+                            </Form.Item>
+                        </Form>
+                    </Flex>
                 </Flex>
             </Flex>
-        </Flex>
+        </div>
     );
 };
--- rms_fe/src/views/Admin/Users/UsersListPage.tsx
+++ rms_fe/src/views/Admin/Users/UsersListPage.tsx
@@ -1,5 +1,5 @@
 import { Button, Empty, Flex, Typography } from "antd"
-import { FileAddOutlined } from "@ant-design/icons"
+import { FileAddOutlined, LeftCircleOutlined } from "@ant-design/icons"
 import { useNavigate } from "react-router-dom"
 import { useQuery } from "@tanstack/react-query"
 import { listUsers } from "../../../clients/users.ts"
@@ -15,8 +15,17 @@ export const UsersListPage = () => {
 
     return (
         <Flex vertical style={{ padding: "2em 4em" }} gap={64}>
-            <Typography.Title> Users </Typography.Title>
-
+            <div>
+                <Button
+                    size="large"
+                    style={{ marginBottom: "1em", maxWidth: "100px" }}
+                    icon={<LeftCircleOutlined />}
+                    onClick={() => navigate(-1)}
+                >
+                    Back
+                </Button>
+                <Typography.Title> Users </Typography.Title>
+            </div>
             {users.data?.length === 0 && <Empty />}
             {users.data?.length !== 0 && <UsersTable isLoading={users.isLoading} users={users.data!} />}
 
